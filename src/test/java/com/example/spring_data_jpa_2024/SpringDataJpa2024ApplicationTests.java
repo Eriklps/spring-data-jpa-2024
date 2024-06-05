72c32ce205407f7e718bc7df1659b0a0c1e105a1
Eriklps <erikdslopes@gmail.com>
2024-06-04 21:31:39 -0300

    Initial commit

---
+++ src/test/java/com/example/spring_data_jpa_2024/SpringDataJpa2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_jpa_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpa2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
